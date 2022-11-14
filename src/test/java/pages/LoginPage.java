fa1b24352a5bdb9f7f63b055abfbd95c63e9656d
Mathan <mathan.chandrasekar@gmail.com>
2022-11-14 10:07:23 +0530

    Day 1 Changes

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import base.BaseClass;
+
+public class LoginPage extends BaseClass{
+	
+	private By oUsernameText = By.id("username");
+	private By oPasswordText = By.id("password");
+	private By oSignInBtn = By.xpath("//button[text()='Sign In']");
+	private By oForgotLink = By.xpath("//*[text()='Register For Account']");
+	private By oRegisterLink = By.xpath("//*[text()='Register For Account']");
+	private By oLoginFailedInnerText = By.xpath("//div[contains(text(),'login failed')]");
+	
+	public boolean verifyElement() {
+		if(driver.findElement(oUsernameText).isDisplayed() && 
+			driver.findElement(oPasswordText).isDisplayed() &&
+			driver.findElement(oSignInBtn).isEnabled() &&
+			driver.findElement(oForgotLink).isDisplayed() &&
+			driver.findElement(oRegisterLink).isDisplayed() &&
+			driver.findElement(oLoginFailedInnerText).isDisplayed()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public LoginPage typeUserName(String userName) {
+		driver.findElement(oUsernameText).sendKeys(userName);
+		return this;
+	}
+	
+	public LoginPage typePassword(String password) {
+		driver.findElement(oPasswordText).sendKeys(password);
+		return this;
+	}
+	
+	public HomePage clickSignOn() {
+		driver.findElement(oSignInBtn).click();
+		return new HomePage();
+	}
+	
+	public LoginPage clickSingOnWithInValidCredential() {
+		driver.findElement(oSignInBtn).click();
+		return this;
+	}
+
+	public boolean validateLoginFailedText() {
+		boolean result = driver.findElement(oLoginFailedInnerText).isDisplayed();
+		return result;
+	}
+	
+
+}
